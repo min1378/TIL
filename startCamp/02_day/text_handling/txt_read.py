5083bec48f2cd85dec2c1c8cedb00081ae3a82ee
min1378 <qwes123@naver.com>
2019-07-09 17:10:37 +0900

    reorganized file

--- startCamp/02_day/text_handling/txt_read.py
+++ startCamp/02_day/text_handling/txt_read.py
@@ -10,4 +10,4 @@ with open('with_ssafy.txt', 'r') as f:
 with open('with_ssafy.txt', 'r') as f:
     lines=f.readlines()
     for line in lines:
-        print(line.strip()) #공백이나 개행을 지워주는 strip
+        print(line.strip()) #공백이나 개행을 지워주는 strip()
